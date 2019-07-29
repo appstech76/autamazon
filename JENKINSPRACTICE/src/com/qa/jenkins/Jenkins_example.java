f45f98c4dfdc988c0ad1defb0a8375f4a649e3fb
Prasad M <Prasad M@DESKTOP-Q3T0OHB>
2019-07-29 09:10:09 +0530

    First build

---
+++ JENKINSPRACTICE/src/com/qa/jenkins/Jenkins_example.java
@@ -0,0 +1,15 @@
+package com.qa.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Jenkins_example {
+
+	@Test
+	public void main()
+	{
+		System.out.println("Hello, Welcome to Jenkins world");
+		
+	}
+	
+	
+}
